82de1de3ad786c43580a6161393a191c4c040626
pirooz <piroozazarabad@gmail.com>
2022-05-16 16:49:38 +0430

    Delete unusable comments and refactor code

--- src/main/java/com/mycompany/onlineexam/OnlineExamApplication.java
+++ src/main/java/com/mycompany/onlineexam/OnlineExamApplication.java
@@ -29,25 +29,4 @@ public class OnlineExamApplication {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    CommandLineRunner run(UserService userService, StudentService studentService, MasterService masterService,
-                          AdminService adminService) {
-        return args -> {
-/*            userService.saveRole(new Role("ROLE_STUDENT"));
-            userService.saveRole(new Role("ROLE_MASTER"));
-            userService.saveRole(new Role("ROLE_ADMIN"));
-
-
-            studentService.createStudent(new StudentDTO(null, "pirooz123", "123", null, "09118249965"));
-            masterService.createMaster(new MasterDTO(null, "master", "123", null, "09118249966"));
-            adminService.saveAdmin(new Admin( "admin", "123", new ArrayList<>(), "admin-code"));
-
-
-            userService.addRoleToUser("pirooz123", "ROLE_STUDENT");
-            userService.addRoleToUser("master", "ROLE_MASTER");
-            userService.addRoleToUser("admin", "ROLE_STUDENT");
-            userService.addRoleToUser("admin", "ROLE_MASTER");
-            userService.addRoleToUser("admin", "ROLE_ADMIN");*/
-        };
-    }
 }
